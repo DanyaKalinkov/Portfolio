366a8e878ba927bcb9c7b865e67dec7106e90dc6
dkaalinkov@yandex.ru <dkaalinkov@yandex.ru>
2025-07-15 19:16:45 +0300

    add purpose gcov_report

--- decimal/src/build/CTestTestfile.cmake
+++ decimal/src/build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(decimal_test "/mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src/build/run_tests")
-set_tests_properties(decimal_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src/CMakeLists.txt;9;add_test;/mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src/CMakeLists.txt;0;")
+set_tests_properties(decimal_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src/CMakeLists.txt;11;add_test;/mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src/CMakeLists.txt;0;")
