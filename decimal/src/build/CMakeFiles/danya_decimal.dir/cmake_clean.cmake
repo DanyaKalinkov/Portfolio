a1800ef6de16c09263dd1b8c383e551b456c11f7
dkaalinkov@yandex.ru <dkaalinkov@yandex.ru>
2025-07-15 17:27:17 +0300

    add Cmake

---
+++ decimal/src/build/CMakeFiles/danya_decimal.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/danya_decimal.dir/danya_additional.c.o"
+  "CMakeFiles/danya_decimal.dir/danya_additional.c.o.d"
+  "CMakeFiles/danya_decimal.dir/danya_decimal.c.o"
+  "CMakeFiles/danya_decimal.dir/danya_decimal.c.o.d"
+  "libdanya_decimal.a"
+  "libdanya_decimal.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/danya_decimal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
