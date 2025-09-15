ce5b7651526d54847db0cacdbe2cb00bbbff8454
dkaalinkov@yandex.ru <dkaalinkov@yandex.ru>
2025-09-15 18:01:03 +0300

    delete build

--- decimal/src/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src
-# Build directory: /mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(decimal_test "/mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src/build/run_tests")
-set_tests_properties(decimal_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src/CMakeLists.txt;13;add_test;/mnt/c/Users/dkaal/Desktop/Portfolio/Portfolio/decimal/src/CMakeLists.txt;0;")
